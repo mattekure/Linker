08fe96fa3095b12637f0d699be479e0acd63a196
mattekure <585663+mattekure@users.noreply.github.com>
2023-12-16 13:09:43 -0500

    Update UI to fix for new FG layout code

---
+++ src/manager_linker.lua
@@ -0,0 +1,11 @@
+function onTabletopInit()
+    local tButton =
+    {
+        sIcon = "linker_button",
+        tooltipres = "linker_title_LabelCaption",
+        class = "linker",
+    };
+    if Session.IsHost then
+        DesktopManager.registerSidebarToolButton(tButton, false);
+    end
+end
