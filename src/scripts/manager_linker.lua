b82c0a464451665f4b86e605ddd6d1f202a28b4d
John Waite <mattekure@gmail.com>
2025-01-26 14:22:57 -0500

    1.5 update
    Update the linker to allow both URL and File paths.  Auto fix file paths
    to work properly.

--- src/scripts/manager_linker.lua
+++ src/scripts/manager_linker.lua
@@ -8,3 +8,15 @@ function onTabletopInit()
 		DesktopManager.registerSidebarToolButton(tButton, false)
 	end
 end
+
+function formatURL(bFile, tURL)
+	if bFile then
+		tURL = string.gsub(tURL, " ", "%%20")
+		if string.sub(tURL, 1, 1) == "/" then
+			tURL = "file://" .. tURL
+		else
+			tURL = "file:///" .. tURL
+		end
+	end
+	return tURL
+end
